a58285e6d6222870bc2cf71179ca1ce2999d3220
Adam Zerner <adamzerner@protonmail.com>
2023-10-30 18:22:11 -0700

    Add home link

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -3,6 +3,7 @@ import { LayoutProps } from "$fresh/server.ts";
 export default ({ Component }: LayoutProps) => {
   return (
     <div class="sm:mx-auto sm:w-[650px] mt-10 text-gray-900 min-h-screen pb-5 sm:pb-3">
+      <a href="/" class="underline mb-8 block">Home</a>
       <Component />
     </div>
   );
