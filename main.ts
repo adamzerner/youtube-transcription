72cc1e38cff68f67ce819ac38a8049bbc4aab38a
Adam Zerner <adamzerner@protonmail.com>
2023-10-30 18:19:18 -0700

    Video transcript

--- main.ts
+++ main.ts
@@ -11,16 +11,3 @@ import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
 await start(manifest, config);
-
-// import { YoutubeTranscript } from "npm:youtube-transcript";
-
-// if (Deno.args.length === 0) {
-//   console.log(
-//     "Please include a video id like so: deno run index.ts WlluO225q0k",
-//   );
-// } else {
-//   const response = await YoutubeTranscript.fetchTranscript(Deno.args[0]);
-//   const transcript = response.map((segment) => segment.text).join(" ");
-
-//   console.log(transcript);
-// }
--- routes/video/[id].tsx
+++ routes/video/[id].tsx
@@ -1,11 +1,31 @@
 import { RouteContext } from "$fresh/server.ts";
+import { YoutubeTranscript } from "npm:youtube-transcript";
 
 export default async (_req: Request, ctx: RouteContext) => {
+  // WlluO225q0k (real video ID)
   const videoId = ctx.params.id;
 
-  return (
-    <div>
-      Transcript for video {videoId}
-    </div>
-  );
+  try {
+    const response = await YoutubeTranscript.fetchTranscript(videoId);
+    const transcript = response.map((segment) => segment.text).join(" ");
+
+    return (
+      <div>
+        <div>
+          Transcript for video with ID of{" "}
+          <span class="font-bold">{videoId}</span>:
+        </div>
+        <div class="mt-6">{transcript}</div>
+      </div>
+    );
+  } catch (e) {
+    return (
+      <div>
+        Unable to fetch transcript of video with ID of{" "}
+        <span class="font-bold">
+          {videoId}
+        </span>.
+      </div>
+    );
+  }
 };
